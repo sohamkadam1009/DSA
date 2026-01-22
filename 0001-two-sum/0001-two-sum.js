3d7e3af5adc30d9d75ef99679cb7059a5e48828d
Soham Kadam <sohamkadam1009@gmail.com>
2026-01-22 18:22:38 +0530

    Time: 0 ms (100%), Space: 55.4 MB (51.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    //brute force soln:time complexity is o(n^2)
+    // for(let i=0;i<nums.length;i++) {
+    //     for(let j=i+1;j<nums.length;j++) {
+    //         const sum = nums[i] + nums[j];
+    //         const index = [i,j];
+    //         if(sum===target) return index; 
+    //     }
+    // }
+
+    const map = new Map();
+
     for(let i=0;i<nums.length;i++) {
-        for(let j=i+1;j<nums.length;j++) {
-            const sum = nums[i] + nums[j];
-            const index = [i,j];
-            if(sum===target) return index; 
-        }
+        //calculate the complement
+        const complement = target - nums[i];
+        if(map.has(complement)) return [map.get(complement),i] //map.get(complement) will return the complemen's index, i-> current index
+        //store the number in the map {number,index}
+        map.set(nums[i],i);
     }
 };
