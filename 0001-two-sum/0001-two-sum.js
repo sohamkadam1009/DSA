3dce5340917f08e7bd7789610d46baf6ba7d9005
Soham Kadam <sohamkadam1009@gmail.com>
2026-01-22 18:09:09 +0530

    Time: 188 ms (5.05%), Space: 62 MB (5.01%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i=0;i<nums.length;i++) {
+        for(let j=i+1;j<nums.length;j++) {
+            const sum = nums[i] + nums[j];
+            const index = [i,j];
+            if(sum===target) return index; 
+        }
+    }
+};
