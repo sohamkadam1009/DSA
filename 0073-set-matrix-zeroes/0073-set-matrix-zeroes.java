22b5c0a8e10464f7a5c1fff99d70fdf6f04251eb
Soham Kadam <sohamkadam1009@gmail.com>
2026-03-23 18:21:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,25 +2,35 @@ class Solution {
     public void setZeroes(int[][] arr) {
         int n =  arr.length;
         int m = arr[0].length;
-        int[] row = new int[n];
-        int[] col = new int[m];
+        int col0 = 1;
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 if(arr[i][j] == 0) {
-                    if(col[j] != 1 || row[i] != 1) {
-                        col[j] = 1;
-                        row[i] = 1;
-                    }
+                    arr[i][0] = 0;
+                    if(j != 0) arr[0][j] = 0;
+                    else col0 = 0;
                 }
             }
         }
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(row[i] == 1 || col[j] == 1) {
+                if(arr[i][0] == 0 || arr[0][j] == 0) {
                     arr[i][j] = 0;
                 }
+            }
+        }
+
+        if(arr[0][0] == 0) {
+            for(int i=1;i<n;i++) {
+                arr[i][0] = 0;
+            }
+        }
+
+        if(col0 == 0) {
+            for(int j=1;j<m;j++) {
+                arr[0][j] = 0;
             }
         }
     }
